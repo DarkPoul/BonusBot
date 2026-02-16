88173038777b237770c1663b044acaa784b3086c
DarkPoul <poulpotter1@gmail.com>
2026-02-16 15:44:20 +0200

    initial

---
+++ src/main/java/esvar/ua/bonusbot/BonusBotApplication.java
@@ -0,0 +1,13 @@
+package esvar.ua.bonusbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonusBotApplication.class, args);
+    }
+
+}
